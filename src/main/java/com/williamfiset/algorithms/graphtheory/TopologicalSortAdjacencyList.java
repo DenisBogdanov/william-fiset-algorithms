0aca4380f20dee0c9f957cc6e0ddd187f0979279
Denis Bogdanov <bogdanov.math@gmail.com>
2022-04-14 20:47:03 +0300

    Learned topological sort.

--- src/main/java/com/williamfiset/algorithms/graphtheory/TopologicalSortAdjacencyList.java
+++ src/main/java/com/williamfiset/algorithms/graphtheory/TopologicalSortAdjacencyList.java
@@ -33,36 +33,38 @@ public class TopologicalSortAdjacencyList {
   // us the topological ordering we want. Instead of maintaining a stack
   // of the nodes we see we simply place them inside the ordering array
   // in reverse order for simplicity.
-  private static int dfs(
-      int i, int at, boolean[] visited, int[] ordering, Map<Integer, List<Edge>> graph) {
+  private static int dfs(int indexToInsertNode, int at, boolean[] visited, int[] ordering,
+                         Map<Integer, List<Edge>> graph) {
 
     visited[at] = true;
 
-    List<Edge> edges = graph.get(at);
-
-    if (edges != null)
-      for (Edge edge : edges) if (!visited[edge.to]) i = dfs(i, edge.to, visited, ordering, graph);
-
-    ordering[i] = at;
-    return i - 1;
+    for (Edge edge : graph.getOrDefault(at, List.of())) {
+      if (!visited[edge.to]) {
+        indexToInsertNode = dfs(indexToInsertNode, edge.to, visited, ordering, graph);
+      }
+    }
+    ordering[indexToInsertNode] = at;
+    return indexToInsertNode - 1;
   }
 
   // Finds a topological ordering of the nodes in a Directed Acyclic Graph (DAG)
   // The input to this function is an adjacency list for a graph and the number
   // of nodes in the graph.
   //
-  // NOTE: 'numNodes' is not necessarily the number of nodes currently present
+  // NOTE: 'nodeSize' is not necessarily the number of nodes currently present
   // in the adjacency list since you can have singleton nodes with no edges which
   // wouldn't be present in the adjacency list but are still part of the graph!
   //
-  public static int[] topologicalSort(Map<Integer, List<Edge>> graph, int numNodes) {
+  public static int[] topologicalSort(Map<Integer, List<Edge>> graph, int nodeSize) {
+    int[] ordering = new int[nodeSize];
+    boolean[] visited = new boolean[nodeSize];
 
-    int[] ordering = new int[numNodes];
-    boolean[] visited = new boolean[numNodes];
-
-    int i = numNodes - 1;
-    for (int at = 0; at < numNodes; at++)
-      if (!visited[at]) i = dfs(i, at, visited, ordering, graph);
+    int indexToInsertNode = nodeSize - 1;
+    for (int at = 0; at < nodeSize; at++) {
+      if (!visited[at]) {
+        indexToInsertNode = dfs(indexToInsertNode, at, visited, ordering, graph);
+      }
+    }
 
     return ordering;
   }
@@ -75,24 +77,22 @@ public class TopologicalSortAdjacencyList {
   // in the adjacency list since you can have singleton nodes with no edges which
   // wouldn't be present in the adjacency list but are still part of the graph!
   //
-  public static Integer[] dagShortestPath(Map<Integer, List<Edge>> graph, int start, int numNodes) {
+  public static Integer[] dagShortestPath(Map<Integer, List<Edge>> graph,
+                                          int start, int numNodes) {
 
-    int[] topsort = topologicalSort(graph, numNodes);
+    int[] topSort = topologicalSort(graph, numNodes);
     Integer[] dist = new Integer[numNodes];
     dist[start] = 0;
 
     for (int i = 0; i < numNodes; i++) {
 
-      int nodeIndex = topsort[i];
+      int nodeIndex = topSort[i];
       if (dist[nodeIndex] != null) {
-        List<Edge> adjacentEdges = graph.get(nodeIndex);
-        if (adjacentEdges != null) {
-          for (Edge edge : adjacentEdges) {
+        for (Edge edge : graph.getOrDefault(nodeIndex, List.of())) {
 
-            int newDist = dist[nodeIndex] + edge.weight;
-            if (dist[edge.to] == null) dist[edge.to] = newDist;
-            else dist[edge.to] = Math.min(dist[edge.to], newDist);
-          }
+          int newDist = dist[nodeIndex] + edge.weight;
+          if (dist[edge.to] == null) dist[edge.to] = newDist;
+          else dist[edge.to] = Math.min(dist[edge.to], newDist);
         }
       }
     }
